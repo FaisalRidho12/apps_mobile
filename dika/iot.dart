0fc7f326b452f17fa961199726bc6dce237a0407
anDK14 <andikaakun14@gmail.com>
2024-10-23 08:15:57 +0700

    update iot

--- dika/iot.dart
+++ dika/iot.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class IoTContent extends StatefulWidget {
   const IoTContent({super.key});
@@ -8,7 +9,21 @@ class IoTContent extends StatefulWidget {
 }
 
 class _IoTContentState extends State<IoTContent> {
-  bool _isDeviceOn = false; // State for IoT device
+  final String _baseUrl =
+      'http://192.168.251.103'; // Replace with your ESP8266 IP address
+
+  Future<void> _moveServo(int position) async {
+    try {
+      final response = await http.get(Uri.parse('$_baseUrl/servo/$position'));
+      if (response.statusCode == 200) {
+        print('Servo moved to $position degrees!');
+      } else {
+        print('Failed to move servo. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +40,32 @@ class _IoTContentState extends State<IoTContent> {
             ),
           ),
           const SizedBox(height: 20),
-          SwitchListTile(
-            title: const Text('Device Status'),
-            value: _isDeviceOn,
-            onChanged: (bool value) {
-              setState(() {
-                _isDeviceOn = value; // Update IoT device state
-                // Implement IoT control logic here
-                print('Device turned ${_isDeviceOn ? "ON" : "OFF"}');
-              });
-            },
-          ),
-          const SizedBox(height: 20),
           // Display an image when the IoT tab is selected
           Image.asset(
-            'assets/images/iot_image.png', // Add your IoT image here
+            'assets/images/image-iot1.png', // Add your IoT image here
             height: 200, // Adjust the height as needed
             fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 20),
+          // Buttons to control the servo
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => _moveServo(0),
+                child: const Text('0°'),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: () => _moveServo(90),
+                child: const Text('90°'),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: () => _moveServo(180),
+                child: const Text('180°'),
+              ),
+            ],
           ),
         ],
       ),
