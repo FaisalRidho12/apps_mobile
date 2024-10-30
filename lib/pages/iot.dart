973131e402a27886bb7801a1b8d9c6da56a45b80
fayt001 <158676327+fayt001@users.noreply.github.com>
2024-10-30 13:12:10 +0700

    update

--- lib/pages/iot.dart
+++ lib/pages/iot.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
-import 'profile.dart'; // Import file profile.dart
+import 'profile.dart'; // Import profile.dart
 import 'package:http/http.dart' as http;
 
 class IoTContent extends StatefulWidget {
@@ -11,9 +11,8 @@ class IoTContent extends StatefulWidget {
 }
 
 class _IoTContentState extends State<IoTContent> {
-  final String _baseUrl =
-      'http://192.168.251.103'; // Ganti dengan alamat IP ESP8266 Anda
-  int _currentIndex = 1; // Indeks untuk IoT di BottomNavigationBar
+  final String _baseUrl = 'http://192.168.1.26'; // Replace with ESP8266 IP
+  int _currentIndex = 1; // Index for IoT in BottomNavigationBar
 
   Future<void> _moveServo(int position) async {
     try {
@@ -28,22 +27,74 @@ class _IoTContentState extends State<IoTContent> {
     }
   }
 
+  void _showFeedDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pilih Posisi Servo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _moveServo(0);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('TUTUP'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  _moveServo(90);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('90°'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  _moveServo(180);
+                  Navigator.of(context).pop();
+                },
+                child: const Text('BUKA'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showMonitoringDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Monitoring'),
+          content: const Text('Monitoring information goes here.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Tutup'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Text(
-          'IoT Control',
-          style: TextStyle(color: Colors.teal, fontSize: 24),
-        ),
-        centerTitle: true,
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const SizedBox(height: 40),
             const Text(
               'Kontrol IoT',
               style: TextStyle(
@@ -54,27 +105,83 @@ class _IoTContentState extends State<IoTContent> {
             ),
             const SizedBox(height: 20),
             Image.asset(
-              'assets/images/image-iot1.png', // Tambahkan gambar IoT Anda di sini
+              'assets/images/image-iot1.png',
               height: 200,
               fit: BoxFit.cover,
             ),
             const SizedBox(height: 20),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 ElevatedButton(
-                  onPressed: () => _moveServo(0),
-                  child: const Text('0°'),
+                  onPressed: _showMonitoringDialog,
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.grey,
+                  ),
+                  child: SizedBox(
+                    width: 120,
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/icons1/monitoring-icon.png',
+                          color: Colors.teal,
+                          height: 50,
+                          width: 50,
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Monitoring',
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                const SizedBox(width: 20),
                 ElevatedButton(
-                  onPressed: () => _moveServo(90),
-                  child: const Text('90°'),
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () => _moveServo(180),
-                  child: const Text('180°'),
+                  onPressed: _showFeedDialog,
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.grey,
+                  ),
+                  child: SizedBox(
+                    width: 120,
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/icons1/makan-icon.png',
+                          color: Colors.teal,
+                          height: 50,
+                          width: 50,
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Pakan',
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -85,7 +192,6 @@ class _IoTContentState extends State<IoTContent> {
     );
   }
 
-  // Fungsi untuk Bottom Navigation Bar
   Widget _buildBottomNavigationBar() {
     return BottomNavigationBar(
       currentIndex: _currentIndex,
@@ -94,18 +200,16 @@ class _IoTContentState extends State<IoTContent> {
           _currentIndex = index;
         });
         if (index == 0) {
-          // Pergi ke Home
           Navigator.pushAndRemoveUntil(
             context,
             MaterialPageRoute(builder: (context) => const HomeScreen()),
-            (Route<dynamic> route) => false, // Hapus stack sebelumnya
+            (Route<dynamic> route) => false,
           );
         } else if (index == 2) {
-          // Pergi ke Profil
           Navigator.pushAndRemoveUntil(
             context,
             MaterialPageRoute(builder: (context) => const ProfileScreen()),
-            (Route<dynamic> route) => false, // Hapus stack sebelumnya
+            (Route<dynamic> route) => false,
           );
         }
       },
