68c4478ea788e4f0f7341a920ba1de638447ead2
fulayyy <159601192+fulayyy@users.noreply.github.com>
2024-11-07 10:42:52 +0700

    Update profile.dart
    baru

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,9 @@
+import 'package:cat_care/autenti/login.dart';
 import 'package:flutter/material.dart';
-import 'home.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home.dart';
 import 'iot.dart';
-import 'aboutus.dart';
-import 'notifikasi.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -12,21 +13,45 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  int _currentIndex = 2; // Default index untuk Profil
+  int _currentIndex = 2;
+  User? user;
+  String displayName = 'Username';
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser; // Ambil data pengguna yang sedang login
+    if (user != null) {
+      _fetchUsername(); // Ambil data nama pengguna dari Firestore
+    }
+  }
+
+  // Fungsi untuk mengambil nama pengguna dari Firestore
+  Future<void> _fetchUsername() async {
+    try {
+      DocumentSnapshot snapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user!.uid)
+          .get();
+      if (snapshot.exists) {
+        setState(() {
+          displayName = snapshot.get('displayName') ?? 'Username';
+        });
+      } else {
+        print('Dokumen tidak ditemukan');
+      }
+    } catch (e) {
+      print('Error fetching username: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.teal),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
         title: const Text(
           'Profile',
-          style: TextStyle(color: Colors.teal, fontSize: 24),
+          style: TextStyle(color: Color(0xFF594545), fontSize: 24),
         ),
         centerTitle: true,
       ),
@@ -34,22 +59,20 @@ class _ProfileScreenState extends State<ProfileScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Bagian Profile User
             const CircleAvatar(
               radius: 40,
-              backgroundImage: AssetImage('assets/images/pp.png'), // Gambar profil
+              backgroundImage: AssetImage('assets/images/pp.png'),
             ),
             const SizedBox(height: 16),
-            const Text(
-              'Username',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.teal),
+            Text(
+              displayName, // Menampilkan nama pengguna
+              style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFF594545)),
             ),
-            const Text(
-              'username@gmail.com',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
+            Text(
+              user?.email ?? 'username@gmail.com', // Menampilkan email pengguna
+              style: const TextStyle(fontSize: 16, color: Colors.grey),
             ),
             const SizedBox(height: 32),
-            // List Menu Pengaturan
             _buildMenuItem(context, Icons.settings, 'Pengaturan', () {
               Navigator.push(
                 context,
@@ -57,18 +80,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
               );
             }),
             _buildMenuItem(context, Icons.notifications, 'Notifikasi', () {
-              // Aksi ketika Notifikasi ditekan
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const NotificationScreen()),
-              );
+              // Logika untuk navigasi ke halaman Notifikasi
             }),
             _buildMenuItem(context, Icons.info_outline, 'Tentang Kami', () {
-              // Aksi ketika Tentang Kami ditekan
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AboutusScreen()),
-                );
+              // Logika untuk navigasi ke halaman Tentang Kami
             }),
           ],
         ),
@@ -77,7 +92,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  // Fungsi untuk membangun item menu
   Widget _buildMenuItem(BuildContext context, IconData icon, String title, VoidCallback onTap) {
     return GestureDetector(
       onTap: onTap,
@@ -97,21 +111,19 @@ class _ProfileScreenState extends State<ProfileScreen> {
         ),
         child: Row(
           children: [
-            Icon(icon, color: Colors.teal),
+            Icon(icon, color: const Color(0xFF594545)),
             const SizedBox(width: 16),
             Text(
               title,
-              style: const TextStyle(fontSize: 18, color: Colors.teal),
+              style: const TextStyle(fontSize: 18, color: Color(0xFF594545)),
             ),
             const Spacer(),
-            const Icon(Icons.arrow_forward_ios, color: Colors.teal),
           ],
         ),
       ),
     );
   }
 
-  // Fungsi untuk Bottom Navigation Bar
   Widget _buildBottomNavigationBar() {
     return BottomNavigationBar(
       currentIndex: _currentIndex,
@@ -120,18 +132,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
           _currentIndex = index;
         });
         if (index == 0) {
-          // Pergi ke Home
           Navigator.pushAndRemoveUntil(
             context,
-            MaterialPageRoute(builder: (context) => const HomeScreen()), // Ambil HomeScreen dari home.dart
-            (Route<dynamic> route) => false, // Hapus stack sebelumnya
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+            (Route<dynamic> route) => false,
           );
         } else if (index == 1) {
-          // Pergi ke IoT
           Navigator.pushAndRemoveUntil(
             context,
-            MaterialPageRoute(builder: (context) => const IoTContent()), // Ambil IotScreen dari home.dart
-            (Route<dynamic> route) => false, // Hapus stack sebelumnya
+            MaterialPageRoute(builder: (context) => const IoTContent()),
+            (Route<dynamic> route) => false,
           );
         }
       },
@@ -149,14 +159,13 @@ class _ProfileScreenState extends State<ProfileScreen> {
           label: 'Profile',
         ),
       ],
-      selectedItemColor: Colors.teal,
+      selectedItemColor: const Color(0xFF594545),
       unselectedItemColor: Colors.grey,
       showUnselectedLabels: true,
     );
   }
 }
 
-// Halaman pengaturan (Settings)
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
@@ -164,15 +173,16 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.teal),
+          icon: const Icon(Icons.arrow_back, color: Color(0xFF594545)),
           onPressed: () => Navigator.of(context).pop(),
         ),
         title: const Text(
           'Akun',
-          style: TextStyle(color: Colors.teal, fontSize: 24),
+          style: TextStyle(color: Color(0xFF594545), fontSize: 24),
         ),
         centerTitle: true,
       ),
@@ -182,29 +192,30 @@ class SettingsScreen extends StatelessWidget {
           children: [
             const CircleAvatar(
               radius: 40,
-              backgroundImage: AssetImage('assets/images/pp.png'), // Gambar profil
+              backgroundImage: AssetImage('assets/images/pp.png'),
             ),
             const SizedBox(height: 16),
             const Text(
               'Username',
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.teal),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFF594545)),
             ),
             const Text(
               'username@gmail.com',
               style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
             const SizedBox(height: 32),
-            _buildAccountMenuItem(Icons.person_add, 'Tambahkan Akun', () {
-              // Aksi Tambahkan Akun
-            }),
+            _buildAccountMenuItem(Icons.person_add, 'Tambahkan Akun', () {}),
             _buildAccountMenuItem(Icons.delete, 'Hapus Akun', () {
-              // Aksi Hapus Akun
+              _showDeleteAccountDialog(context);
             }),
-            _buildAccountMenuItem(Icons.edit, 'Edit Akun', () {
-              // Aksi Edit Akun
+            _buildAccountMenuItem(Icons.edit, 'Ganti Password', () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ChangePasswordWidget()),
+              );
             }),
             _buildAccountMenuItem(Icons.logout, 'Logout', () {
-              // Aksi Logout
+              _showLogoutConfirmationDialog(context);
             }),
           ],
         ),
@@ -231,16 +242,109 @@ class SettingsScreen extends StatelessWidget {
         ),
         child: Row(
           children: [
-            Icon(icon, color: Colors.teal),
+            Icon(icon, color: Color(0xFF594545)),
             const SizedBox(width: 16),
             Text(
               title,
-              style: const TextStyle(fontSize: 18, color: Colors.teal),
+              style: const TextStyle(fontSize: 18, color: Color(0xFF594545)),
             ),
             const Spacer(),
-            const Icon(Icons.arrow_forward_ios, color: Colors.teal),
+            const Icon(Icons.arrow_forward_ios, color: Color(0xFF594545)),
           ],
         ),
+      ),
+    );
+  }
+
+  void _showDeleteAccountDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Hapus Akun'),
+          content: const Text('Apakah Anda yakin ingin menghapus akun ini?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop();
+
+                final user = FirebaseAuth.instance.currentUser;
+
+                if (user != null) {
+                  try {
+                    // Hapus data pengguna dari Firestore
+                    await FirebaseFirestore.instance
+                        .collection('users') // Sesuaikan dengan koleksi di Firestore
+                        .doc(user.uid)
+                        .delete();
+
+                    // Hapus akun pengguna dari Firebase Authentication
+                    await user.delete();
+
+                    // Sign out pengguna dari aplikasi
+                    await FirebaseAuth.instance.signOut();
+
+                    // Arahkan ke layar login setelah akun berhasil dihapus
+                  } catch (e) {
+                    print("Error menghapus akun: $e");
+                  }
+                }
+              },
+              child: const Text('Hapus'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Logout'),
+          content: const Text('Apakah Anda yakin ingin keluar?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  (Route<dynamic> route) => false,
+                );
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ChangePasswordWidget extends StatelessWidget {
+  const ChangePasswordWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ganti Password'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: const Text('Halaman Ganti Password'),
       ),
     );
   }
